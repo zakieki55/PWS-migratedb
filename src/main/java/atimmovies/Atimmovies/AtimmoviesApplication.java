e11bb9374f3be6dcb38abc575215bc35df368c56
zakieki55 <zakueki@gmail.com>
2022-10-18 22:59:28 +0700

    add plugin maven

---
+++ src/main/java/atimmovies/Atimmovies/AtimmoviesApplication.java
@@ -0,0 +1,13 @@
+package atimmovies.Atimmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtimmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtimmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atimmovies/Atimmovies/AtimmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package atimmovies.Atimmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtimmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
