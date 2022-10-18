a3f9d2c928dc55d712bf41997f3da855be25946d
zakieki55 <zakueki@gmail.com>
2022-10-18 23:07:22 +0700

    Change Persistence

--- src/main/java/atimmovies/Atimmovies/PelangganJpaController.java
+++ src/main/java/atimmovies/Atimmovies/PelangganJpaController.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -26,7 +27,7 @@ public class PelangganJpaController implements Serializable {
     public PelangganJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("atimmovies_Atimmovies_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
